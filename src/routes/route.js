f351225e720f8b168ab0aa08e5542bb5b339e494
unknown <richasharma535270@gmail.com>
2022-05-03 23:47:48 +0530

    final

--- src/routes/route.js
+++ src/routes/route.js
@@ -2,13 +2,12 @@ const express = require("express");
 const router = express.Router();
 const blogController = require("../Controllers/blogController");
 const authorController = require("../Controllers/authorController");
-const { route } = require("express/lib/application");
 const loginController = require("../Controllers/loginController");
 const auth = require("../Middlewares/auth");
 
 router.post("/authors", authorController.createAuthor);
-router.post("/CreateBlog", auth.authentication, blogController.CreateBlog);
-router.get("/GetData", auth.authentication, blogController.GetData);
+router.post("/blogs", auth.authentication, blogController.CreateBlog);
+router.get("/blogs", auth.authentication, blogController.GetData);
 router.put("/blogs/:blogId", auth.authentication,auth.authorization,blogController.updateBlog);
 router.delete("/blogs/:blogId",auth.authentication,auth.authorization,blogController.deleteBlog);
 router.delete("/blogs",auth.authentication, blogController.deleteQuery);
